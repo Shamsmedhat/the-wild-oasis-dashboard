f87161f61b4d2df43c88b0d233134816529d706e
Shams <shamsmedhat1@gmail.com>
2024-12-07 18:06:59 +0200

    Fix supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://cbhskjvapedefwrhfhef.supabase.co";
+export const supabaseUrl = "https://ziqhehuuhhygzczpdtce.supabase.co";
 const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiaHNranZhcGVkZWZ3cmhmaGVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2OTk3OTksImV4cCI6MjAyODI3NTc5OX0.6EU_2Z1FOQQ4oUFfUT9cqMO87xqMmmwlEHm0a3fdcs0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppcWhlaHV1aGh5Z3pjenBkdGNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NzEwMjQsImV4cCI6MjA0OTE0NzAyNH0.FobFuHVjCt3Jw76qirPv2Iv7QGNKJiDIpytaOtInXBQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
